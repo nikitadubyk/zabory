2e5fbd9d0c8537580df768694407779be557143c
Nikita <example@mail.ru>
2021-01-30 15:58:30 +0300

    all ready!

--- src/js/script.js
+++ src/js/script.js
@@ -15,3 +15,49 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+// плавный скролл
+var $page = $('html, body');
+$('a[href*="#"]').click(function() {
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 400);
+    return false;
+});
+
+
+$('.modal__close').on('click', function () {
+    $('.overlay, #thanks').fadeOut('slow');
+});
+
+//phpmailer
+$('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+        $('.overlay, #thanks').fadeIn('slow');
+
+        $('form').trigger('reset');
+    });
+    return false;
+});
+// $(document).ready(function(){
+//     $('form').submit(function(e) {
+//         e.preventDefault();
+//         $.ajax({
+//             type: "POST",
+//             url: "mailer/smart.php",
+//             data: $(this).serialize()
+//         }).done(function() {
+//             $(this).find("input").val("");
+//             $('.overlay, #thanks').fadeIn('slow');
+
+//             $('form').trigger('reset');
+//         });
+//         return false;
+//     });
+// });
