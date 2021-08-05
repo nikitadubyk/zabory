526a6fa2f0f129c11ab765df02e6bac38be31da9
Nikita <nikita_dubyk@mail.ru>
2021-08-05 12:59:23 +0300

    fixed script.js, promo.scss and hover on blocks

--- src/js/script.js
+++ src/js/script.js
@@ -19,15 +19,24 @@ window.addEventListener('DOMContentLoaded', () => {
 // плавный скролл
 var $page = $('html, body');
 $('a[href*="#"]').click(function() {
+    const menuList = document.querySelector('.menu__list_active'),
+          hamburger = document.querySelector('.hamburger_active');
+
     $page.animate({
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 400);
+
+    if (menuList) {
+        hamburger.classList.toggle('hamburger_active');
+        menuList.classList.toggle('menu__list_active');
+    }
     return false;
 });
 
 
 $('.modal__close').on('click', function () {
     $('.overlay, #thanks').fadeOut('slow');
+    document.body.style.overflow = ''; 
 });
 
 //phpmailer
@@ -40,24 +49,8 @@ $('form').submit(function(e) {
     }).done(function() {
         $(this).find("input").val("");
         $('.overlay, #thanks').fadeIn('slow');
-
+        document.body.style.overflow = 'hidden'; 
         $('form').trigger('reset');
     });
     return false;
 });
-// $(document).ready(function(){
-//     $('form').submit(function(e) {
-//         e.preventDefault();
-//         $.ajax({
-//             type: "POST",
-//             url: "mailer/smart.php",
-//             data: $(this).serialize()
-//         }).done(function() {
-//             $(this).find("input").val("");
-//             $('.overlay, #thanks').fadeIn('slow');
-
-//             $('form').trigger('reset');
-//         });
-//         return false;
-//     });
-// });
